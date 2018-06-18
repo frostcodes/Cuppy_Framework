bc5d2c2fc0ad29705f278cf82005a0a9f5a6a95c
Seyi Aderinkomi <seyiaderinkomi@gmail.com>
2018-06-18 16:51:47 +0100

    Added Material checkbox control

---
+++ MaterialCheckBox.bas
@@ -0,0 +1,160 @@
+﻿Type=Class
+Version=5.51
+ModulesStructureVersion=1
+B4J=true
+@EndOfDesignText@
+'Custom View class
+ 
+#Event: CheckedChanged(value as int)
+ 
+ 'TODO: make font adjust when resized
+#Region Internal Segment
+
+Sub Class_Globals
+	Private fx As JFX
+	Private mEventName As String 'ignore
+	Private mCallBack As Object 'ignore
+	Private mBase As Pane
+ 
+	Private CheckedLabel As Label
+	Private CheckboxPane As Pane
+	
+	'Checkbox states
+	Public CHECKED_STATE As Int = 0 
+	Public UNCHECKED_STATE As Int = 1
+	Public INDETERMINATE_STATE As Int = 2
+	
+	
+	
+End Sub
+
+Public Sub Initialize (Callback As Object, EventName As String)
+	mEventName = EventName
+	mCallBack = Callback
+End Sub
+
+Public Sub DesignerCreateView (Base As Pane, Lbl As Label, Props As Map)
+	mBase = Base
+	mBase.LoadLayout("MaterialCheckboxLayout")
+	'set using theme...
+	SetBg(StyleManager.DefaultTheme.Get("divider"))
+	setCheckedColor(StyleManager.DefaultTheme.Get("primary"))
+	
+	'TODO: create a designer prorerty for this...
+	setCheckState(UNCHECKED_STATE) 'set initial value
+	
+End Sub
+
+Private Sub Base_Resize (Width As Double, Height As Double)
+ 
+	CheckboxPane.PrefWidth = Width
+	CheckedLabel.PrefWidth = Width
+	
+	CheckboxPane.PrefHeight = Height
+	CheckedLabel.PrefHeight = Height
+	 
+End Sub
+
+Public Sub GetBase As Pane
+	Return mBase
+End Sub
+
+#End Region
+
+#Region Actions and Effects
+
+Public Sub SetBg(color As String)
+ 
+	CSSUtils.SetStyleProperty( CheckboxPane, "-fx-background-color", color)
+ 
+End Sub
+
+Public Sub setRotationX(angle As Float)
+	
+	ControlsUtils.setPaneRotationX(CheckboxPane, angle) 'rotate
+	 
+End Sub
+
+Public Sub setBorder(color As String , width As Int)
+	
+	ControlsUtils.setPaneBorder(CheckboxPane, color, width)
+
+End Sub
+ 
+Public Sub setBorderRadius(radius As Int)
+	
+	ControlsUtils.setPaneBorderRadius(CheckboxPane, radius)
+	
+End Sub
+ 
+Public Sub setPaneEffect(effect As String)
+	
+	ControlsUtils.setPaneEffect(CheckboxPane, effect)
+	
+End Sub
+
+Public Sub removeEffects()
+	
+	ControlsUtils.removePaneEffect(CheckboxPane)
+	
+End Sub
+
+#End Region
+  
+Public Sub setCheckedColor(color As String)
+  	
+	CSSUtils.SetStyleProperty( CheckedLabel, "-fx-background-color", color)
+	
+End Sub
+
+Public Sub setCheckState(value As Int)
+	 
+	If value = UNCHECKED_STATE Then
+		
+		CheckedLabel.Visible = False
+		CheckedLabel.SetAlphaAnimated(300, 0 )
+		
+		 
+	Else if value = CHECKED_STATE Then
+	
+		CheckedLabel.Visible = True
+		CheckedLabel.SetAlphaAnimated(300, 1 )
+		 
+		Else
+			
+		CheckedLabel.SetAlphaAnimated(300, 0.6 )
+		CheckedLabel.Visible = True
+		 	
+	End If
+	 
+	'call callback for checked changed status
+	 CallSubDelayed2(mCallBack, mEventName & "_CheckedChanged" , value)
+	 
+End Sub
+
+Public Sub checked As Boolean
+
+	Return CheckedLabel.Visible
+	 
+End Sub
+
+Public Sub Alpha As Double
+	
+	Return CheckedLabel.Alpha
+	
+End Sub
+  
+Sub CheckboxPane_MousePressed (EventData As MouseEvent)
+	 
+	If Not(checked) Or Alpha = "0.6" Then
+	
+		setCheckState(CHECKED_STATE)
+	
+	Else
+	
+ 	setCheckState(UNCHECKED_STATE)
+	 	  
+	End If
+	
+	
+End Sub
---
+++ Objects/src/Punchline/Tech/Cuppy/Framework/materialcheckbox.java
@@ -0,0 +1,215 @@
+package Punchline.Tech.Cuppy.Framework;
+
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.B4AClass;
+import anywheresoftware.b4a.debug.*;
+
+public class materialcheckbox extends B4AClass.ImplB4AClass implements BA.SubDelegator{
+    public static java.util.HashMap<String, java.lang.reflect.Method> htSubs;
+    private void innerInitialize(BA _ba) throws Exception {
+        if (ba == null) {
+            ba = new  anywheresoftware.b4j.objects.FxBA("Punchline.Tech.Cuppy.Framework", "Punchline.Tech.Cuppy.Framework.materialcheckbox", this);
+            if (htSubs == null) {
+                ba.loadHtSubs(this.getClass());
+                htSubs = ba.htSubs;
+            }
+            ba.htSubs = htSubs;
+             
+        }
+        if (BA.isShellModeRuntimeCheck(ba))
+                this.getClass().getMethod("_class_globals", Punchline.Tech.Cuppy.Framework.materialcheckbox.class).invoke(this, new Object[] {null});
+        else
+            ba.raiseEvent2(null, true, "class_globals", false);
+    }
+
+ public anywheresoftware.b4a.keywords.Common __c = null;
+public anywheresoftware.b4j.objects.JFX _fx = null;
+public String _meventname = "";
+public Object _mcallback = null;
+public anywheresoftware.b4j.objects.PaneWrapper.ConcretePaneWrapper _mbase = null;
+public anywheresoftware.b4j.objects.LabelWrapper _checkedlabel = null;
+public anywheresoftware.b4j.objects.PaneWrapper.ConcretePaneWrapper _checkboxpane = null;
+public int _checked_state = 0;
+public int _unchecked_state = 0;
+public int _indeterminate_state = 0;
+public b4j.example.cssutils _cssutils = null;
+public Punchline.Tech.Cuppy.Framework.configs _configs = null;
+public Punchline.Tech.Cuppy.Framework.componentsmanager _componentsmanager = null;
+public Punchline.Tech.Cuppy.Framework.stylemanager _stylemanager = null;
+public Punchline.Tech.Cuppy.Framework.controlsutils _controlsutils = null;
+public Punchline.Tech.Cuppy.Framework.animationmanager _animationmanager = null;
+public Punchline.Tech.Cuppy.Framework.licensemanager _licensemanager = null;
+public double  _alpha() throws Exception{
+ //BA.debugLineNum = 137;BA.debugLine="Public Sub Alpha As Double";
+ //BA.debugLineNum = 139;BA.debugLine="Return CheckedLabel.Alpha";
+if (true) return _checkedlabel.getAlpha();
+ //BA.debugLineNum = 141;BA.debugLine="End Sub";
+return 0;
+}
+public String  _base_resize(double _width,double _height) throws Exception{
+ //BA.debugLineNum = 43;BA.debugLine="Private Sub Base_Resize (Width As Double, Height A";
+ //BA.debugLineNum = 45;BA.debugLine="CheckboxPane.PrefWidth = Width";
+_checkboxpane.setPrefWidth(_width);
+ //BA.debugLineNum = 46;BA.debugLine="CheckedLabel.PrefWidth = Width";
+_checkedlabel.setPrefWidth(_width);
+ //BA.debugLineNum = 48;BA.debugLine="CheckboxPane.PrefHeight = Height";
+_checkboxpane.setPrefHeight(_height);
+ //BA.debugLineNum = 49;BA.debugLine="CheckedLabel.PrefHeight = Height";
+_checkedlabel.setPrefHeight(_height);
+ //BA.debugLineNum = 51;BA.debugLine="End Sub";
+return "";
+}
+public String  _checkboxpane_mousepressed(anywheresoftware.b4j.objects.NodeWrapper.MouseEventWrapper _eventdata) throws Exception{
+ //BA.debugLineNum = 143;BA.debugLine="Sub CheckboxPane_MousePressed (EventData As MouseE";
+ //BA.debugLineNum = 145;BA.debugLine="If Not(checked) Or Alpha = \"0.6\" Then";
+if (__c.Not(_checked()) || _alpha()==(double)(Double.parseDouble("0.6"))) { 
+ //BA.debugLineNum = 147;BA.debugLine="setCheckState(CHECKED_STATE)";
+_setcheckstate(_checked_state);
+ }else {
+ //BA.debugLineNum = 151;BA.debugLine="setCheckState(UNCHECKED_STATE)";
+_setcheckstate(_unchecked_state);
+ };
+ //BA.debugLineNum = 156;BA.debugLine="End Sub";
+return "";
+}
+public boolean  _checked() throws Exception{
+ //BA.debugLineNum = 131;BA.debugLine="Public Sub checked As Boolean";
+ //BA.debugLineNum = 133;BA.debugLine="Return CheckedLabel.Visible";
+if (true) return _checkedlabel.getVisible();
+ //BA.debugLineNum = 135;BA.debugLine="End Sub";
+return false;
+}
+public String  _class_globals() throws Exception{
+ //BA.debugLineNum = 8;BA.debugLine="Sub Class_Globals";
+ //BA.debugLineNum = 9;BA.debugLine="Private fx As JFX";
+_fx = new anywheresoftware.b4j.objects.JFX();
+ //BA.debugLineNum = 10;BA.debugLine="Private mEventName As String 'ignore";
+_meventname = "";
+ //BA.debugLineNum = 11;BA.debugLine="Private mCallBack As Object 'ignore";
+_mcallback = new Object();
+ //BA.debugLineNum = 12;BA.debugLine="Private mBase As Pane";
+_mbase = new anywheresoftware.b4j.objects.PaneWrapper.ConcretePaneWrapper();
+ //BA.debugLineNum = 14;BA.debugLine="Private CheckedLabel As Label";
+_checkedlabel = new anywheresoftware.b4j.objects.LabelWrapper();
+ //BA.debugLineNum = 15;BA.debugLine="Private CheckboxPane As Pane";
+_checkboxpane = new anywheresoftware.b4j.objects.PaneWrapper.ConcretePaneWrapper();
+ //BA.debugLineNum = 18;BA.debugLine="Public CHECKED_STATE As Int = 0";
+_checked_state = (int) (0);
+ //BA.debugLineNum = 19;BA.debugLine="Public UNCHECKED_STATE As Int = 1";
+_unchecked_state = (int) (1);
+ //BA.debugLineNum = 20;BA.debugLine="Public INDETERMINATE_STATE As Int = 2";
+_indeterminate_state = (int) (2);
+ //BA.debugLineNum = 24;BA.debugLine="End Sub";
+return "";
+}
+public String  _designercreateview(anywheresoftware.b4j.objects.PaneWrapper.ConcretePaneWrapper _base,anywheresoftware.b4j.objects.LabelWrapper _lbl,anywheresoftware.b4a.objects.collections.Map _props) throws Exception{
+ //BA.debugLineNum = 31;BA.debugLine="Public Sub DesignerCreateView (Base As Pane, Lbl A";
+ //BA.debugLineNum = 32;BA.debugLine="mBase = Base";
+_mbase = _base;
+ //BA.debugLineNum = 33;BA.debugLine="mBase.LoadLayout(\"MaterialCheckboxLayout\")";
+_mbase.LoadLayout(ba,"MaterialCheckboxLayout");
+ //BA.debugLineNum = 35;BA.debugLine="SetBg(StyleManager.DefaultTheme.Get(\"divider\"))";
+_setbg(BA.ObjectToString(_stylemanager._defaulttheme.Get((Object)("divider"))));
+ //BA.debugLineNum = 36;BA.debugLine="setCheckedColor(StyleManager.DefaultTheme.Get(\"pr";
+_setcheckedcolor(BA.ObjectToString(_stylemanager._defaulttheme.Get((Object)("primary"))));
+ //BA.debugLineNum = 39;BA.debugLine="setCheckState(UNCHECKED_STATE) 'set initial value";
+_setcheckstate(_unchecked_state);
+ //BA.debugLineNum = 41;BA.debugLine="End Sub";
+return "";
+}
+public anywheresoftware.b4j.objects.PaneWrapper.ConcretePaneWrapper  _getbase() throws Exception{
+ //BA.debugLineNum = 53;BA.debugLine="Public Sub GetBase As Pane";
+ //BA.debugLineNum = 54;BA.debugLine="Return mBase";
+if (true) return _mbase;
+ //BA.debugLineNum = 55;BA.debugLine="End Sub";
+return null;
+}
+public String  _initialize(anywheresoftware.b4a.BA _ba,Object _callback,String _eventname) throws Exception{
+innerInitialize(_ba);
+ //BA.debugLineNum = 26;BA.debugLine="Public Sub Initialize (Callback As Object, EventNa";
+ //BA.debugLineNum = 27;BA.debugLine="mEventName = EventName";
+_meventname = _eventname;
+ //BA.debugLineNum = 28;BA.debugLine="mCallBack = Callback";
+_mcallback = _callback;
+ //BA.debugLineNum = 29;BA.debugLine="End Sub";
+return "";
+}
+public String  _removeeffects() throws Exception{
+ //BA.debugLineNum = 91;BA.debugLine="Public Sub removeEffects()";
+ //BA.debugLineNum = 93;BA.debugLine="ControlsUtils.removePaneEffect(CheckboxPane)";
+_controlsutils._removepaneeffect(_checkboxpane);
+ //BA.debugLineNum = 95;BA.debugLine="End Sub";
+return "";
+}
+public String  _setbg(String _color) throws Exception{
+ //BA.debugLineNum = 61;BA.debugLine="Public Sub SetBg(color As String)";
+ //BA.debugLineNum = 63;BA.debugLine="CSSUtils.SetStyleProperty( CheckboxPane, \"-fx-bac";
+_cssutils._setstyleproperty((anywheresoftware.b4j.objects.NodeWrapper.ConcreteNodeWrapper) anywheresoftware.b4a.AbsObjectWrapper.ConvertToWrapper(new anywheresoftware.b4j.objects.NodeWrapper.ConcreteNodeWrapper(), (javafx.scene.Node)(_checkboxpane.getObject())),"-fx-background-color",_color);
+ //BA.debugLineNum = 65;BA.debugLine="End Sub";
+return "";
+}
+public String  _setborder(String _color,int _width) throws Exception{
+ //BA.debugLineNum = 73;BA.debugLine="Public Sub setBorder(color As String , width As In";
+ //BA.debugLineNum = 75;BA.debugLine="ControlsUtils.setPaneBorder(CheckboxPane, color,";
+_controlsutils._setpaneborder(_checkboxpane,_color,_width);
+ //BA.debugLineNum = 77;BA.debugLine="End Sub";
+return "";
+}
+public String  _setborderradius(int _radius) throws Exception{
+ //BA.debugLineNum = 79;BA.debugLine="Public Sub setBorderRadius(radius As Int)";
+ //BA.debugLineNum = 81;BA.debugLine="ControlsUtils.setPaneBorderRadius(CheckboxPane, r";
+_controlsutils._setpaneborderradius(_checkboxpane,_radius);
+ //BA.debugLineNum = 83;BA.debugLine="End Sub";
+return "";
+}
+public String  _setcheckedcolor(String _color) throws Exception{
+ //BA.debugLineNum = 99;BA.debugLine="Public Sub setCheckedColor(color As String)";
+ //BA.debugLineNum = 101;BA.debugLine="CSSUtils.SetStyleProperty( CheckedLabel, \"-fx-bac";
+_cssutils._setstyleproperty((anywheresoftware.b4j.objects.NodeWrapper.ConcreteNodeWrapper) anywheresoftware.b4a.AbsObjectWrapper.ConvertToWrapper(new anywheresoftware.b4j.objects.NodeWrapper.ConcreteNodeWrapper(), (javafx.scene.Node)(_checkedlabel.getObject())),"-fx-background-color",_color);
+ //BA.debugLineNum = 103;BA.debugLine="End Sub";
+return "";
+}
+public String  _setcheckstate(int _value) throws Exception{
+ //BA.debugLineNum = 105;BA.debugLine="Public Sub setCheckState(value As Int)";
+ //BA.debugLineNum = 107;BA.debugLine="If value = UNCHECKED_STATE Then";
+if (_value==_unchecked_state) { 
+ //BA.debugLineNum = 109;BA.debugLine="CheckedLabel.Visible = False";
+_checkedlabel.setVisible(__c.False);
+ //BA.debugLineNum = 110;BA.debugLine="CheckedLabel.SetAlphaAnimated(300, 0 )";
+_checkedlabel.SetAlphaAnimated((int) (300),0);
+ }else if(_value==_checked_state) { 
+ //BA.debugLineNum = 115;BA.debugLine="CheckedLabel.Visible = True";
+_checkedlabel.setVisible(__c.True);
+ //BA.debugLineNum = 116;BA.debugLine="CheckedLabel.SetAlphaAnimated(300, 1 )";
+_checkedlabel.SetAlphaAnimated((int) (300),1);
+ }else {
+ //BA.debugLineNum = 120;BA.debugLine="CheckedLabel.SetAlphaAnimated(300, 0.6 )";
+_checkedlabel.SetAlphaAnimated((int) (300),0.6);
+ //BA.debugLineNum = 121;BA.debugLine="CheckedLabel.Visible = True";
+_checkedlabel.setVisible(__c.True);
+ };
+ //BA.debugLineNum = 127;BA.debugLine="CallSubDelayed2(mCallBack, mEventName & \"_Checked";
+__c.CallSubDelayed2(ba,_mcallback,_meventname+"_CheckedChanged",(Object)(_value));
+ //BA.debugLineNum = 129;BA.debugLine="End Sub";
+return "";
+}
+public String  _setpaneeffect(String _effect) throws Exception{
+ //BA.debugLineNum = 85;BA.debugLine="Public Sub setPaneEffect(effect As String)";
+ //BA.debugLineNum = 87;BA.debugLine="ControlsUtils.setPaneEffect(CheckboxPane, effect)";
+_controlsutils._setpaneeffect(_checkboxpane,_effect);
+ //BA.debugLineNum = 89;BA.debugLine="End Sub";
+return "";
+}
+public String  _setrotationx(float _angle) throws Exception{
+ //BA.debugLineNum = 67;BA.debugLine="Public Sub setRotationX(angle As Float)";
+ //BA.debugLineNum = 69;BA.debugLine="ControlsUtils.setPaneRotationX(CheckboxPane, angl";
+_controlsutils._setpanerotationx(_checkboxpane,_angle);
+ //BA.debugLineNum = 71;BA.debugLine="End Sub";
+return "";
+}
+public Object callSub(String sub, Object sender, Object[] args) throws Exception {
+BA.senderHolder.set(sender);
+return BA.SubDelegator.SubNotFound;
+}
+}
